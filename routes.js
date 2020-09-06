9ef59c6c8d4a671ace07a64bd07966361740001e
sohyeonpark0901 <shg5865@naver.com>
2020-09-07 02:05:31 +0900

    heroku hello world

---
+++ routes.js
@@ -0,0 +1,41 @@
+const router = require("express").Router();
+const multer = require("multer");
+const fs = require("fs");
+const path = require("path");
+const { error } = require("console");
+
+// const { spawn } = require("child_process");
+// const pyProg = spawn("python", ["C:/Users/PK/AppData/Local/Programs/Python/Python37-32/get_avg.py"]);
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "../../sample/media/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+router.post("/uploadfile", upload.array("userfile", 2), function (req, res) {
+  res.json({ ans: "success" });
+});
+
+router.get("/machine", function (req, res) {
+  try {
+    const { spawn } = require("child_process");
+    const pyProg = spawn("python", ["C:/Users/PK/AppData/Local/Programs/Python/Python37-32/get_avg.py"]);
+
+    pyProg.stdout.on("data", function (data) {
+      console.log(data);
+      console.log(data.toString());
+      res.write(data);
+      res.end();
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+const myRouter = require("./routes");
+var ip = require("ip");
+let http = require("http");
+let https = require("https");
+const fs = require("fs");
+middlewareLoad(app);
+app.use("/vertify", myRouter);
+
+var options = {
+  key: fs.readFileSync("./key.pem", "utf8"),
+  cert: fs.readFileSync("./server.crt", "utf8"),
+};
+
+// http.createServer(app).listen(8080, function () {
+//   console.log("http://" + ip.address() + ":" + 8080 + "| start time : " + new Date());
+// });
+https.createServer(options, app).listen(port, function () {
+  console.log("https://" + ip.address() + ":" + port + "| start time : " + new Date());
+});
+
+(function () {
+  var childProcess = require("child_process");
+  var oldSpawn = childProcess.spawn;
+  function mySpawn() {
+    console.log("spawn called");
+    console.log(arguments);
+    var result = oldSpawn.apply(this, arguments);
+    return result;
+  }
+  childProcess.spawn = mySpawn;
+})();
+
+// app.listen(9000, function () {
+//   console.log("server start");
+// });
