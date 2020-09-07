a322f013efa4b57faa3e146a4756330a710d9eff
sohyeonpark0901 <shg5865@naver.com>
2020-09-07 17:10:05 +0900

    python change

--- middleware.js
+++ middleware.js
@@ -4,7 +4,7 @@ const path = require("path");
 const bodyParser = require("body-parser");
 function middlewareLoad(app) {
   app.use(express.static(path.join("public")));
-  app.use("/vertify/fileSVM", express.static(path.join("uploads")));
+  app.use("/vertify/uploadfile", express.static(path.join("uploads")));
   app.use(express.json());
   app.use(express.urlencoded({ extended: true })); //json 객체안에 또 객체가 있는경우 파싱해주기 위해
   app.use(bodyParser.urlencoded({ extended: true })); //application/x=www-form-urlencoded 파싱
--- routes.js
+++ routes.js
@@ -3,19 +3,18 @@ const multer = require("multer");
 const fs = require("fs");
 const path = require("path");
 const { error } = require("console");
-var pathOfIndex = path.relative("C:/Users/PK/Desktop/졸업작품/server/", "C:/Users/PK/AppData/Local/Programs/Python/Python37-32/get_avg.py");
+var pathOfIndex = path.relative("C:/Users/PK/Desktop/졸업작품/server/", "C:/Users/PK/Desktop/졸업작품/server/get_avg.py");
 // const { spawn } = require("child_process");
 // const pyProg = spawn("python", ["C:/Users/PK/AppData/Local/Programs/Python/Python37-32/get_avg.py"]);
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "../../sample/media/");
+    cb(null, "uploads/");
   },
   filename: (req, file, cb) => {
     cb(null, file.originalname);
   },
 });
-
 const upload = multer({ storage });
 
 router.post("/uploadfile", upload.array("userfile", 2), function (req, res) {
