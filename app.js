d827ec9c842d9d30682c59c2575f5c9331235c12
sohyeonpark0901 <shg5865@naver.com>
2020-09-16 01:03:50 +0900

    port change

--- app.js
+++ app.js
@@ -18,9 +18,9 @@ var options = {
   cert: fs.readFileSync("./server.crt", "utf8"),
 };
 
-http.createServer(app).listen(80, function () {
-  console.log("http://" + ip.address() + ":" + 80 + "| start time : " + new Date());
-});
+// http.createServer(app).listen(80, function () {
+//   console.log("http://" + ip.address() + ":" + 80 + "| start time : " + new Date());
+// });
 // https.createServer(options, app).listen(9000, function (req, res) {
 //   console.log("https://" + ip.address() + ":" + 9000 + "| start time : " + new Date());
 // });
@@ -37,6 +37,6 @@ http.createServer(app).listen(80, function () {
   childProcess.spawn = mySpawn;
 })();
 
-// app.listen(9000, function () {
-//   console.log("server start");
-// });
+app.listen(9000, function () {
+  console.log("server start");
+});
